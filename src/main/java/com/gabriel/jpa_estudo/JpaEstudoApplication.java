e00946fc07f475377297319d8d25c9f22d467423
Gabriel Uchoa <gabrieluchoa2607@gmail.com>
2026-04-24 16:04:02 -0300

    chore: Spring Boot Setup

---
+++ src/main/java/com/gabriel/jpa_estudo/JpaEstudoApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.jpa_estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaEstudoApplication.class, args);
+	}
+
+}
