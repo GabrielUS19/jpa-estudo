0d32eb097b8ffe938d692c4f0b178093738bf0fc
Gabriel Uchoa <gabrieluchoa2607@gmail.com>
2026-04-24 17:57:18 -0300

    feat: Implements the models Book, Author, Publisher and Review

---
+++ src/main/java/com/gabriel/jpa_estudo/models/AuthorModel.java
@@ -0,0 +1,47 @@
+package com.gabriel.jpa_estudo.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "authors")
+public class AuthorModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToMany(mappedBy = "authors", fetch = FetchType.LAZY)
+    private Set<BookModel> books = new HashSet<>();
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<BookModel> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<BookModel> books) {
+        this.books = books;
+    }
+}
